c35a134a4a2033750e1300aa378a2fb30889fb26
Javier Odera <javier.odera@correo.ucu.edu.uy>
2022-10-31 19:31:06 -0300

    hice mi propio objeto porque faltaron mis compañeros

---
+++ src/Library/items/Mjolnour.cs
@@ -0,0 +1,17 @@
+namespace RoleplayGame.Items;
+
+public class Mjolnour : IAttackItem
+{
+    public int AttackPower // ponemos el poder de ataque
+    {
+        get
+        {
+            return 30;
+        }
+    }
+    
+    public override string ToString()//ponemos lo que se escribira si llamamos al objeto
+        {
+            return "Robes";
+        }
+}
